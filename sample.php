d1353e37b355b9a15ae0c4393f7ae2cc87b490f5
RenHagino <reol0405@gmail.com>
2020-02-01 18:16:19 +0900

    sample.phpには4行の文章があります

--- sample.php
+++ sample.php
@@ -1,4 +1,7 @@
 <?php 
 echo 'こんにちは';
 echo '変更が確認できましたか？？？？？';
+echo 'git add . の後、git commitの前にgit diff --cached ファイル名で
+  以前コミットしたものとの差分を確認してみましょう';
+echo 'git statusでも確認してみましょう。to be committedがありますか？';
 ?>
